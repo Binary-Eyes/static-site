afaefcdcbce5f5acdfce0a13b23e639f850e9c94
amir_barak <binary.eyes.studio@gmail.com>
2025-06-04 01:02:58 +0300

    tested converting bold text node

--- src/test_textnode_to_htmlnode.py
+++ src/test_textnode_to_htmlnode.py
@@ -3,6 +3,13 @@ from textnode import *
 from htmlnode import *
 
 class TestTextNodeToHtmlNode(unittest.TestCase):
+    def test_bold_text(self):
+        text_node = TextNode("bold text node", TextType.BOLD)
+        html_node = text_node_to_html_node(text_node)
+        expected = "<b>bold text node</b>"
+        self.assertEqual(expected, html_node.to_html())
+    
+    
     def test_plain_text_conversion(self):
         text_node = TextNode("simple text node", TextType.TEXT)
         html_node = text_node_to_html_node(text_node)

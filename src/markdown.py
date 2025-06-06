6f15953082b661ffebb8dfe210e7fef645a5f346
amir_barak <binary.eyes.studio@gmail.com>
2025-06-06 20:16:55 +0300

    testing markdown splitter

--- src/markdown.py
+++ src/markdown.py
@@ -1,2 +1,30 @@
+from textnode import *
+
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
-    pass
+    if text_type == TextType.TEXT:
+        return old_nodes
+    
+    new_nodes = []
+    for old_node in old_nodes:
+        if old_node.text_type != TextType.TEXT:
+            new_nodes.append(old_node)
+            continue
+
+        first_split = old_node.text.split(delimiter, 1)
+        if len(first_split) != 2:
+            new_nodes.append(old_node)
+            continue
+
+        last_split = first_split[1].split(delimiter, 1)
+        if len(last_split) != 2:
+            raise Exception("invalid markdown: missing closing delimiter")
+        
+        new_nodes.append(TextNode(first_split[0], TextType.TEXT))
+        new_nodes.append(TextNode(last_split[0], text_type))
+        new_nodes.append(TextNode(last_split[1], TextType.TEXT))
+
+    return new_nodes
+        
+
+
+
--- src/test_markdown.py
+++ src/test_markdown.py
@@ -4,6 +4,12 @@ from markdown import *
 from textnode import *
 
 class TestMarkdownSplitter(unittest.TestCase):
+    def test_text_with_single_code_element(self):
+        root = TextNode("the following is a `code snippet` word block", TextType.TEXT)
+        split = split_nodes_delimiter([root], "`", TextType.CODE)
+        self.assertEqual(3, len(split))
+
+    
     def test_simple_text(self):
         root = TextNode("this is a simple text node", TextType.TEXT)
         split = split_nodes_delimiter([root], "", TextType.TEXT)

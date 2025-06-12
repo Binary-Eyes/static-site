38bd773841ef0e02220a07a387d984c00b311b09
amir_barak <binary.eyes.studio@gmail.com>
2025-06-13 02:28:58 +0300

    main will remove public

--- src/main.py
+++ src/main.py
@@ -1,5 +1,13 @@
+import os
+import shutil
+
 print("static site generator v1.0.0")
 print("developed by: amir barak")
+
+public_path = os.path.join(os.curdir, "public")
+if os.path.exists(public_path):
+    print("deleting public")
+    shutil.rmtree(public_path)
 
 
 

2bd8bd11897a4241d408ff094766b96ef83fd548
amir_barak <binary.eyes.studio@gmail.com>
2025-06-12 18:19:13 +0300

    tested multiple paragraphs

--- src/test_markdown_to_html.py
+++ src/test_markdown_to_html.py
@@ -3,6 +3,18 @@ from markdown import *
 from htmlnode import *
 
 class TestMarkdownToHtml(unittest.TestCase):
+    def test_multiple_paragraphs(self):
+        markdown = """
+it was the best of times
+
+it was the worst of times
+"""
+        html_node = markdown_to_html_node(markdown)
+        html = html_node.to_html()
+        expected = "<div><p>it was the best of times</p><p>it was the worst of times</p></div>"
+        self.assertEqual(expected, html)
+        
+    
     def test_multiple_line_paragraph(self):
         markdown = """
 this is the first line
@@ -12,7 +24,7 @@ this is the second line
         html = html_node.to_html()
         expected = "<div><p>this is the first line\nthis is the second line</p></div>"
         self.assertEqual(expected, html)
-        
+
     
     def test_single_line_paragraph(self):
         markdown = """

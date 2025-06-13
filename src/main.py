f35e5bd514ba9c548a6819c453e7ca4c868841af
amir_barak <binary.eyes.studio@gmail.com>
2025-06-13 14:31:14 +0300

    updating static to public process

--- src/main.py
+++ src/main.py
@@ -1,14 +1,45 @@
 import os
 import shutil
 
-print("static site generator v1.0.0")
-print("developed by: amir barak")
+SOURCE = "static"
+TARGET = "public"
 
-public_path = os.path.join(os.curdir, "public")
-if os.path.exists(public_path):
-    print("deleting public")
-    shutil.rmtree(public_path)
+def main():
+    print("static site generator v1.0.0")
+    print("developed by: amir barak")
+
+    static_dir, public_dir = initialize()
+
+    clone_contents(static_dir)
+    print("goodbye")
 
 
+def initialize():
+    static_dir = os.path.join(os.curdir, SOURCE)
+    if not os.path.exists(static_dir):
+        raise Exception(f"static directory missing from current location: {static_dir}")
 
-print("goodbye")
+    public_dir = os.path.join(os.curdir, TARGET)
+    if os.path.exists(public_dir):
+        print(f"deleting public dir: {public_dir}")
+        shutil.rmtree(public_dir)
+    
+    return static_dir, public_dir
+
+
+def clone_contents(source_dir):
+    global SOURCE
+    global TARGET
+    target_dir = source_dir[len(SOURCE):]
+    print(f"{source_dir} ---> {target_dir}")
+
+    # dir_items = os.listdir(parent_dir)
+    # for item in dir_items:
+    #     subpath = os.path.join(parent_dir, item)
+    #     if os.path.isdir(subpath):
+    #         subpath += "/"
+
+    #     print(f" {subpath}")
+
+if __name__ == '__main__':
+    main()
